7df7c013688af4a3ba31cd47a4adda2a11b48e29
Muhammad Usman <m.usman.akram2000@gmail.com>
2021-10-18 02:57:38 +0500

    navigation with routers

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -6,6 +6,7 @@ import { makeStyles } from "@material-ui/styles";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/logo.svg";
 
@@ -67,11 +68,36 @@ export default function Header(props) {
               className={classes.tabContainer}
               indicatorColor='primary'
             >
-              <Tab className={classes.tab} label='Home' />
-              <Tab className={classes.tab} label='Services' />
-              <Tab className={classes.tab} label='The Revolution' />
-              <Tab className={classes.tab} label='About Us' />
-              <Tab className={classes.tab} label='Contact Us' />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/'
+                label='Home'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/services'
+                label='Services'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/revolution'
+                label='The Revolution'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='about'
+                label='About Us'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='contact'
+                label='Contact Us'
+              />
             </Tabs>
             <Button
               variant='contained'
