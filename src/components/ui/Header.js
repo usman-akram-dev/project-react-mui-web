ddf82aee4f12ed394e8cf9ae5e20f2a484645d15
Muhammad Usman <m.usman.akram2000@gmail.com>
2021-10-18 02:44:43 +0500

    styling active tab

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
@@ -49,13 +49,24 @@ const useStyles = makeStyles(theme => ({
 
 export default function Header(props) {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
+
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar>
           <Toolbar disableGutters>
             <img alt='company logo' className={classes.logo} src={logo} />
-            <Tabs className={classes.tabContainer}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor='primary'
+            >
               <Tab className={classes.tab} label='Home' />
               <Tab className={classes.tab} label='Services' />
               <Tab className={classes.tab} label='The Revolution' />
