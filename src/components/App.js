4426772de849007d4de09cddf19373a42386aa7c
Muhammad Usman <m.usman.akram2000@gmail.com>
2021-10-18 01:01:00 +0500

    appbar and styling-theme applied

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Header from "../components/ui/Header";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "./ui/Theme";
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      Hello!
+    </ThemeProvider>
+  );
+}
+
+export default App;
