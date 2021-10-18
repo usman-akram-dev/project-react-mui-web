8079d4a88e35601adc79b549553625d942539e11
Muhammad Usman <m.usman.akram2000@gmail.com>
2021-10-18 14:40:23 +0500

    using menus - refresh fixed

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -12,6 +12,7 @@ import MenuItem from "@material-ui/core/MenuItem";
 
 
 import logo from "../../assets/logo.svg";
+import { Switch } from "@material-ui/core";
 
 
 function ElevationScroll(props) {
@@ -123,19 +124,93 @@ export default function Header(props) {
 
   }
   useEffect(() => {
-    if (window.location.pathname === "/" && value !== 0) {
-      setValue(0);
-    } else if (window.location.pathname === "/services" && value !== 1) {
-      setValue(1);
-    } else if (window.location.pathname === "/revolution" && value !== 2) {
-      setValue(2);
-    } else if (window.location.pathname === "/about" && value !== 3) {
-      setValue(3);
-    } else if (window.location.pathname === "/contact" && value !== 4) {
-      setValue(4);
-    } else if (window.location.pathname === "/estimate" && value !== 5) {
-      setValue(5);
+    // if (window.location.pathname === "/" && value !== 0) {
+    //   setValue(0);
+    // } else if (window.location.pathname === "/services" && value !== 1) {
+    //   setValue(1);
+    // } else if (window.location.pathname === "/revolution" && value !== 2) {
+    //   setValue(2);
+    // } else if (window.location.pathname === "/about" && value !== 3) {
+    //   setValue(3);
+    // } else if (window.location.pathname === "/contact" && value !== 4) {
+    //   setValue(4);
+    // } else if (window.location.pathname === "/estimate" && value !== 5) {
+    //   setValue(5);
+    // }
+
+    switch(window.location.pathname){
+
+      case "/":
+      if(value!==0){
+        setValue(0);
+      }
+      break;
+
+      case "/services":
+        if(value!==1){
+          setValue(1);
+          setSelectedIndex(0);
+
+        }
+        break;
+
+      case "/customsoftware":
+        if(value!==1){
+          setValue(1);
+          setSelectedIndex(1);
+        }
+        break;
+
+      case "/mobileapps":
+          if(value!==1){
+            setValue(1);
+            setSelectedIndex(2);
+          }
+          break;
+
+      case "/websites":
+            if(value!==1){
+              setValue(1);
+              setSelectedIndex(3);
+            }
+            break;
+      case "/revolution":
+              if(value!==2){
+                setValue(2);
+              }
+              break;
+
+      case "/about":
+                  if(value!==3){
+                    setValue(3);
+                  }
+                  break;
+
+      case "/contact":
+                    if(value!==4){
+                      setValue(4);
+                    }
+                    break;
+      
+    
+  
+
+
+
+        default:
+          break;
+
+
+
+
+
+
     }
+
+
+
+
+
   }, [value]);
 
   return (
