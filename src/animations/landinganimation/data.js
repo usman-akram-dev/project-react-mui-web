b68b14d69b3f6b412e4aa71b36f83bfa5bce129b
Muhammad Usman <m.usman.akram2000@gmail.com>
2021-10-23 00:51:21 +0500

    creating the call to action

--- src/animations/landinganimation/data.js
+++ src/animations/landinganimation/data.js
@@ -3,7 +3,7 @@ import img_1 from "./img_1.png";
 import img_2 from "./img_2.png";
 import img_3 from "./img_3.png";
 import img_4 from "./img_4.png";
-
+// eslint-disable-next-line
 export default {
   v: "5.5.5",
   fr: 29.9700012207031,
