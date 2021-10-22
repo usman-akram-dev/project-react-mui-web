f40a362b7fb795d9d12e2be9f221711c3b18c079
Muhammad Usman <m.usman.akram2000@gmail.com>
2021-10-22 15:00:49 +0500

    creating the services block - ios/mobile app development

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -9,7 +9,7 @@ import useMediaQuery  from "@material-ui/core/useMediaQuery";
 import ButtonArrow from "./ui/ButtonArrow";
 import animationData from "../animations/landinganimation/data";
 import customSoftwareIcon from "../assets/Custom Software Icon.svg";
-
+import mobileAppsIcon from "../assets/mobileIcon.svg"
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -138,13 +138,11 @@ export default function LandingPage() {
         </Grid>
       </Grid>
       <Grid item>
-        {/*-----Services Block-----*/}
+        {/*-----Custom Software Block-----*/}
         <Grid container direction="row" className={classes.serviceContainer} justify={matchesSM ? "center" : undefined}>
           <Grid item style={{marginLeft: matchesSM ? 0 : "5em" , textAlign : matchesSM ? "center" : undefined}} >
             <Typography variant="h4">Custom Software Development</Typography>
             <Typography variant="subtitle1" className={classes.subtitle}>Save Energy , Save Time , Save Money.</Typography>
-         
-         
             <Typography variant="subtitle1">Complete digital solutions , from investigation to{" "} <span className={classes.specialText}>celebration</span></Typography>
           <Button variant="outlined" className={classes.learnButton}><span style={{ marginRight: 10 }}>Learn More</span>    
           <ButtonArrow
@@ -156,6 +154,26 @@ export default function LandingPage() {
           </Grid>
           <Grid item>
           <img className={classes.icon} alt="customSoftwareIcon" src={customSoftwareIcon} />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        {/*-----iOS/Android Block-----*/}
+        <Grid container direction="row" className={classes.serviceContainer} justify={matchesSM ? "center" : "flex-end"}>
+          <Grid item style={{textAlign : matchesSM ? "center" : undefined}} >
+            <Typography variant="h4">iOS/Android App Development</Typography>
+            <Typography variant="subtitle1" className={classes.subtitle}>Extend Functionality, Extend Access, Increase Engagement.</Typography>
+            <Typography variant="subtitle1">Integrate your web experience or create a standalone app {matchesSM ? null : <br/> } with either mobile platform</Typography>
+          <Button variant="outlined" className={classes.learnButton}><span style={{ marginRight: 10 }}>Learn More</span>    
+          <ButtonArrow
+                    width="10"
+                    height="10"
+                    fill={theme.palette.common.blue}
+                  />
+                  </Button>
+          </Grid>
+          <Grid item style={{marginRight: matchesSM ? 0 : "5em"  }}>
+          <img className={classes.icon} alt="mobile phone icon" src={mobileAppsIcon} />
           </Grid>
         </Grid>
       </Grid>
--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -114,7 +114,7 @@ export default function Footer(props) {
                   props.setSelectedIndex(2);
                 }}
               >
-                Mobile App Development
+                iOS/Android App Development
               </Grid>
               <Grid
                 item
